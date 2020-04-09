e2769792decdbd97bf0bc0b495514f60d2c44346
David Neal <david@reverentgeek.com>
2020-04-09 16:02:02 -0400

    initial commit

---
+++ src/functions/public-test/public-test.js
@@ -0,0 +1,16 @@
+"use strict";
+
+exports.handler = async ( event, context ) => {
+	try {
+		const subject = event.queryStringParameters.name || "World";
+		return {
+			statusCode: 200,
+			body: JSON.stringify( { message: `Hello ${ subject }` } )
+			// // more keys you can return:
+			// headers: { "headerName": "headerValue", ... },
+			// isBase64Encoded: true,
+		};
+	} catch ( err ) {
+		return { statusCode: 500, body: err.toString() };
+	}
+};
